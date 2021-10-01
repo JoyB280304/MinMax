2b586685dc490c8795300a9027a6de349ade671d
Joy Beshiri <JOY.BESHIRI@local.ittcesena.it>
2021-10-01 09:31:47 +0200

    v1.1: aggiunta algoritmo

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -10,6 +10,21 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5] { 3, 6, -3, 5, 7 };
+
+            int min = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            Console.WriteLine("[{0}]", min);
+
+            Console.ReadKey();
         }
     }
 }
